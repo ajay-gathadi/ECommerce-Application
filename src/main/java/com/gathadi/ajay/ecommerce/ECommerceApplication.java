94410a29c22ec59af7534c690e61f535459dd876
Ajay Gathadi <ajaygathadi@gmail.com>
2026-03-22 15:00:12 +0530

    Ecommerce application initial commit

---
+++ src/main/java/com/gathadi/ajay/ecommerce/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.gathadi.ajay.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceApplication.class, args);
+    }
+
+}
