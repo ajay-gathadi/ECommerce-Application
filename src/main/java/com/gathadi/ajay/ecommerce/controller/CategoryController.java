36ca3f600dffed3810cb902ca1f9ea9e446d1565
Ajay Gathadi <ajaygathadi@gmail.com>
2026-04-04 12:36:07 +0530

    Update the type of deleteCategory method to ResponseEntity to properly format the message for invalid deletion of category which may not be present

--- src/main/java/com/gathadi/ajay/ecommerce/controller/CategoryController.java
+++ src/main/java/com/gathadi/ajay/ecommerce/controller/CategoryController.java
@@ -3,7 +3,10 @@ package com.gathadi.ajay.ecommerce.controller;
 import com.gathadi.ajay.ecommerce.model.Category;
 import com.gathadi.ajay.ecommerce.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,9 +29,13 @@ public class CategoryController {
     }
 
     @DeleteMapping("/api/admin/categories/{categoryId}")
-    public String deleteCategory(@PathVariable("categoryId") Long categoryId) {
-        categoryService.deleteCategory(categoryId);
-        return "Category successfully deleted";
+    public ResponseEntity<String> deleteCategory(@PathVariable("categoryId") Long categoryId) {
+        try {
+            String status = categoryService.deleteCategory(categoryId);
+            return new ResponseEntity<String>(status, HttpStatus.OK);
+        } catch (ResponseStatusException rse) {
+            return new ResponseEntity<String>(rse.getReason(), rse.getStatusCode());
+        }
     }
 
 }
