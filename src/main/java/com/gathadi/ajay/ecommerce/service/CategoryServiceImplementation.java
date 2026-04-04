62de6135e45dc1dec1456d837b9ed77d17876691
Ajay Gathadi <ajaygathadi@gmail.com>
2026-04-04 17:47:59 +0530

    Add updateCategory method to update the categoryName using categoryId

--- src/main/java/com/gathadi/ajay/ecommerce/service/CategoryServiceImplementation.java
+++ src/main/java/com/gathadi/ajay/ecommerce/service/CategoryServiceImplementation.java
@@ -5,14 +5,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.swing.text.html.Option;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
-public class CategoryServiceImplementation implements CategoryService{
+public class CategoryServiceImplementation implements CategoryService {
 
-    private List<Category> categories = new ArrayList<>();
-    public long nextId = 1 ;
+    private final List<Category> categories = new ArrayList<>();
+    public long nextId = 1;
 
     @Override
     public List<Category> getAllCategories() {
@@ -32,5 +34,21 @@ public class CategoryServiceImplementation implements CategoryService{
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
         }
         return "Category with categoryId: " + categoryId + " deleted successfully.";
+    }
+
+    @Override
+    public Category updateCategory(Category category, Long categoryId) {
+
+        Optional<Category> optionalCategory = categories.stream()
+                .filter(currentCategory -> currentCategory.getCategoryId().equals(categoryId))
+                .findFirst();
+
+        if (optionalCategory.isPresent()) {
+            Category currentCategory = optionalCategory.get();
+            currentCategory.setCategoryName(category.getCategoryName());
+            return currentCategory;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
+        }
     }
 }
