0348d921bcfff742dc9c16adb7cf4d0fc7b8260c
Ajay Gathadi <ajaygathadi@gmail.com>
2026-04-04 17:48:13 +0530

    Add updateCategory method

--- src/main/java/com/gathadi/ajay/ecommerce/service/CategoryService.java
+++ src/main/java/com/gathadi/ajay/ecommerce/service/CategoryService.java
@@ -8,4 +8,5 @@ public interface CategoryService {
     public List<Category> getAllCategories();
     void createCategory(Category category);
     String deleteCategory(Long categoryId);
+    Category updateCategory(Category category, Long categoryId);
 }
