03a551248453a64ae3e2f89b907f5f4295aa044c
Ajay Gathadi <ajaygathadi@gmail.com>
2026-04-04 18:21:29 +0530

    Add the class level request mapping at an endpoint /api, and remove the /api at method level

--- src/main/java/com/gathadi/ajay/ecommerce/controller/CategoryController.java
+++ src/main/java/com/gathadi/ajay/ecommerce/controller/CategoryController.java
@@ -13,12 +13,13 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api")
 public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
 
-    @GetMapping("/api/public/categories")
+    @GetMapping("/public/categories")
     public ResponseEntity<List<Category>> getAllCategories() {
         List<Category> allCategories = categoryService.getAllCategories();
 
@@ -29,13 +30,13 @@ public class CategoryController {
         return new ResponseEntity<>(allCategories, HttpStatus.OK);
     }
 
-    @PostMapping("/api/public/categories")
+    @PostMapping("/public/categories")
     public ResponseEntity<String> createCategory(@RequestBody Category category) {
         categoryService.createCategory(category);
         return new ResponseEntity<>("Category Added successfully", HttpStatus.CREATED);
     }
 
-    @PutMapping("/api/public/categories/{categoryId}")
+    @PutMapping("/public/categories/{categoryId}")
     public ResponseEntity<String> updateCategory(@RequestBody Category category, @PathVariable Long categoryId) {
 
         try {
@@ -48,7 +49,7 @@ public class CategoryController {
 
     }
 
-    @DeleteMapping("/api/admin/categories/{categoryId}")
+    @DeleteMapping("/admin/categories/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
         try {
             String status = categoryService.deleteCategory(categoryId);
