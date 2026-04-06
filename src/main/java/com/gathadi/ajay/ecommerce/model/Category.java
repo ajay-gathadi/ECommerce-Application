8edf9acd79b6f562ecba77e33f69602bda6b517a
Ajay Gathadi <ajaygathadi@gmail.com>
2026-04-06 12:09:30 +0530

    Mark the class as Entity and create automatic incremental ids in the database

--- src/main/java/com/gathadi/ajay/ecommerce/model/Category.java
+++ src/main/java/com/gathadi/ajay/ecommerce/model/Category.java
@@ -1,8 +1,18 @@
 package com.gathadi.ajay.ecommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
+
+    public Category() {}
 
     public Category(Long categoryId, String categoryName) {
         this.categoryId = categoryId;
