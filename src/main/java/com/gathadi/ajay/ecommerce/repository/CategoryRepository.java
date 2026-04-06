67b18846392a086bb859223a8a745ed7226f8253
Ajay Gathadi <ajaygathadi@gmail.com>
2026-04-06 13:00:00 +0530

    Add CategoryRepository as a repository

---
+++ src/main/java/com/gathadi/ajay/ecommerce/repository/CategoryRepository.java
@@ -0,0 +1,13 @@
+package com.gathadi.ajay.ecommerce.repository;
+
+import com.gathadi.ajay.ecommerce.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+    Spring sees this CategoryRepository repository interface extending JpaRepository,
+    creates a proxy implementation at runtime,
+    and that proxy executes all the database operations on my behalf —
+    so I never have to write the implementation manually.
+ */
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
