42a01b565d6b3d8d75baa0a03e6ad7907e794184
Ajay Gathadi <ajaygathadi@gmail.com>
2026-04-25 17:14:15 +0530

    Make error message identifiable

--- src/main/java/com/gathadi/ajay/ecommerce/model/Category.java
+++ src/main/java/com/gathadi/ajay/ecommerce/model/Category.java
@@ -18,6 +18,6 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
 
-    @NotBlank(message = "Category name is required")
+    @NotBlank(message = "ERROR: Category name is required")
     private String categoryName;
 }
