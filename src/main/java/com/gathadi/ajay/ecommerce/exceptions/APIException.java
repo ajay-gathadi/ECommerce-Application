c8c416704209e1aea69a02a02e1e48f0739d0cd5
Ajay Gathadi <ajaygathadi@gmail.com>
2026-04-27 14:03:53 +0530

    Update the APIException to receive the appropriate status code to show for the request

--- src/main/java/com/gathadi/ajay/ecommerce/exceptions/APIException.java
+++ src/main/java/com/gathadi/ajay/ecommerce/exceptions/APIException.java
@@ -1,15 +1,24 @@
 package com.gathadi.ajay.ecommerce.exceptions;
 
+import org.springframework.http.HttpStatus;
+
 import java.io.Serial;
 
 public class APIException extends RuntimeException {
     @Serial
     private static final long serialVersionUID = 1L;
 
+    private HttpStatus statusCode;
+
     public APIException() {
     }
 
-    public APIException(String message) {
+    public APIException(String message, HttpStatus statusCode) {
         super(message);
+        this.statusCode = statusCode;
+    }
+
+    public HttpStatus getStatusCode() {
+        return statusCode;
     }
 }
