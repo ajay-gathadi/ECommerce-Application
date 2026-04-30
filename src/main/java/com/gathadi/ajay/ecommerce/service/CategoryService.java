1955e78ca8a22b35b7b99c31df6a8ead285cf8d5
Ajay Gathadi <ajaygathadi@gmail.com>
2026-04-30 11:51:12 +0530

    Update the type of the getAllCategories method from List<Category> to CategoryResponse

--- src/main/java/com/gathadi/ajay/ecommerce/service/CategoryService.java
+++ src/main/java/com/gathadi/ajay/ecommerce/service/CategoryService.java
@@ -1,11 +1,12 @@
 package com.gathadi.ajay.ecommerce.service;
 
 import com.gathadi.ajay.ecommerce.model.Category;
+import com.gathadi.ajay.ecommerce.payload.CategoryResponse;
 
 import java.util.List;
 
 public interface CategoryService {
-    public List<Category> getAllCategories();
+    public CategoryResponse getAllCategories();
     void createCategory(Category category);
     String deleteCategory(Long categoryId);
     Category updateCategory(Category category, Long categoryId);
