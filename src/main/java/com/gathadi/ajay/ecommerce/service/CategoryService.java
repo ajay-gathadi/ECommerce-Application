e10d3d0225e8969f7602895d9bee98bf0967607b
Ajay Gathadi <ajaygathadi@gmail.com>
2026-04-30 17:49:19 +0530

    Update the createCategory method and parameter type to CategoryDTO from Category

--- src/main/java/com/gathadi/ajay/ecommerce/service/CategoryService.java
+++ src/main/java/com/gathadi/ajay/ecommerce/service/CategoryService.java
@@ -1,13 +1,12 @@
 package com.gathadi.ajay.ecommerce.service;
 
 import com.gathadi.ajay.ecommerce.model.Category;
+import com.gathadi.ajay.ecommerce.payload.CategoryDTO;
 import com.gathadi.ajay.ecommerce.payload.CategoryResponse;
-
-import java.util.List;
 
 public interface CategoryService {
     public CategoryResponse getAllCategories();
-    void createCategory(Category category);
+    CategoryDTO createCategory(CategoryDTO category);
     String deleteCategory(Long categoryId);
     Category updateCategory(Category category, Long categoryId);
 }
