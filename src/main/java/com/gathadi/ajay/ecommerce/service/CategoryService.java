8b2d358ebc47f597e5d5068548c2dd80c9cd8e5e
Ajay Gathadi <ajaygathadi@gmail.com>
2026-05-11 15:25:24 +0530

    Request sortBy and sortOrder parametber in the getAllCategories method

--- src/main/java/com/gathadi/ajay/ecommerce/service/CategoryService.java
+++ src/main/java/com/gathadi/ajay/ecommerce/service/CategoryService.java
@@ -2,9 +2,10 @@ package com.gathadi.ajay.ecommerce.service;
 
 import com.gathadi.ajay.ecommerce.payload.CategoryDTO;
 import com.gathadi.ajay.ecommerce.payload.CategoryResponse;
+import org.springframework.http.HttpStatus;
 
 public interface CategoryService {
-    public CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize);
+    public CategoryResponse getAllCategories(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
     CategoryDTO createCategory(CategoryDTO category);
     CategoryDTO deleteCategory(Long categoryId);
     CategoryDTO updateCategory(CategoryDTO categoryDTO, Long categoryId);
