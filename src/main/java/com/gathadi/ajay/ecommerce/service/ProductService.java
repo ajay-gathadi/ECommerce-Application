4a74308e06a417e61a76d401161cfebf42c09848
Ajay Gathadi <ajaygathadi@gmail.com>
2026-05-24 14:45:45 +0530

    Add addProduct method to ProductService interface

--- src/main/java/com/gathadi/ajay/ecommerce/service/ProductService.java
+++ src/main/java/com/gathadi/ajay/ecommerce/service/ProductService.java
@@ -1,9 +1,10 @@
 package com.gathadi.ajay.ecommerce.service;
 
+import com.gathadi.ajay.ecommerce.model.Product;
+import com.gathadi.ajay.ecommerce.payload.ProductDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface ProductService {
-
-    public ProductDTO addProduct(Product product, Long category);
+    ProductDTO addProduct(Product product, Long category);
 }
