e6e004f3903b319caa0e7c894bec3590b73440d6
Ajay Gathadi <ajaygathadi@gmail.com>
2026-05-24 14:43:47 +0530

    Add addProduct method to add the product under a specific category

--- src/main/java/com/gathadi/ajay/ecommerce/controller/ProductController.java
+++ src/main/java/com/gathadi/ajay/ecommerce/controller/ProductController.java
@@ -1,4 +1,25 @@
 package com.gathadi.ajay.ecommerce.controller;
 
+import com.gathadi.ajay.ecommerce.model.Product;
+import com.gathadi.ajay.ecommerce.payload.ProductDTO;
+import com.gathadi.ajay.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
 public class ProductController {
+    @Autowired
+    private ProductService  productService;
+
+    @PostMapping("/admin/categories/{categoryId}/product")
+    public ResponseEntity<ProductDTO> addProduct(@RequestBody Product product, @PathVariable Long categoryId){
+
+        ProductDTO productToBeSaved = productService.addProduct(product, categoryId);
+
+        return new ResponseEntity<>(productToBeSaved, HttpStatus.CREATED);
+    }
+
 }
