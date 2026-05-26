28388a3e719d93047324081d46abaa0646ebd121
Ajay Gathadi <ajaygathadi@gmail.com>
2026-05-26 17:49:12 +0530

    Add getAllProducts method

--- src/main/java/com/gathadi/ajay/ecommerce/service/ProductService.java
+++ src/main/java/com/gathadi/ajay/ecommerce/service/ProductService.java
@@ -2,9 +2,12 @@ package com.gathadi.ajay.ecommerce.service;
 
 import com.gathadi.ajay.ecommerce.model.Product;
 import com.gathadi.ajay.ecommerce.payload.ProductDTO;
+import com.gathadi.ajay.ecommerce.payload.ProductResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface ProductService {
     ProductDTO addProduct(Product product, Long category);
+
+    ProductResponse getAllProducts();
 }
