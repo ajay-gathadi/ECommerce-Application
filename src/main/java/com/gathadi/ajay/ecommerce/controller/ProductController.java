eca5439f086ab03001ca3cc376daa5f95dba5fe9
Ajay Gathadi <ajaygathadi@gmail.com>
2026-05-27 10:29:26 +0530

    Add productByCategory endpoint and return Product lists

--- src/main/java/com/gathadi/ajay/ecommerce/controller/ProductController.java
+++ src/main/java/com/gathadi/ajay/ecommerce/controller/ProductController.java
@@ -29,4 +29,10 @@ public class ProductController {
         return new ResponseEntity<>(productResponse, HttpStatus.OK);
     }
 
+    @GetMapping("/public/categories/{categoryId}/products")
+    public ResponseEntity<ProductResponse> getProductsByCategory(@PathVariable Long categoryId){
+        ProductResponse productResponse = productService.getProductsByCategory(categoryId);
+        return new ResponseEntity<>(productResponse, HttpStatus.OK);
+    }
+
 }
