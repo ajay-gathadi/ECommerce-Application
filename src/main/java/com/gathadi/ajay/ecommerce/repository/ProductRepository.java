be360659a4d04d66ec6c77a86c7eb6604950211c
Ajay Gathadi <ajaygathadi@gmail.com>
2026-05-27 10:30:04 +0530

    Add method to find products by category and order by price

--- src/main/java/com/gathadi/ajay/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/gathadi/ajay/ecommerce/repository/ProductRepository.java
@@ -1,7 +1,12 @@
 package com.gathadi.ajay.ecommerce.repository;
 
+import com.gathadi.ajay.ecommerce.model.Category;
 import com.gathadi.ajay.ecommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategoryOrderByProductPriceAsc(Category category);
 }
+
