34214c94b52b5d1e0a676b078f903f89fb28653d
Ajay Gathadi <ajaygathadi@gmail.com>
2026-05-29 18:31:22 +0530

    Add searchProductsByKeyword method

--- src/main/java/com/gathadi/ajay/ecommerce/service/ProductService.java
+++ src/main/java/com/gathadi/ajay/ecommerce/service/ProductService.java
@@ -12,4 +12,6 @@ public interface ProductService {
     ProductResponse getAllProducts();
 
     ProductResponse getProductsByCategory(Long categoryId);
+
+    ProductResponse searchProductsByKeyword(String keyword);
 }
