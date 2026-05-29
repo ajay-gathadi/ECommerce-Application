7040aaa3a1bd9cb4037c39b25f10bd3046e295e9
Ajay Gathadi <ajaygathadi@gmail.com>
2026-05-29 18:30:39 +0530

    Add findByProductNameLikeIgnoreCase to fetch the products by product names from database

--- src/main/java/com/gathadi/ajay/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/gathadi/ajay/ecommerce/repository/ProductRepository.java
@@ -8,5 +8,7 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByCategoryOrderByProductPriceAsc(Category category);
+
+    List<Product> findByProductNameLikeIgnoreCase(String keyword);
 }
 
