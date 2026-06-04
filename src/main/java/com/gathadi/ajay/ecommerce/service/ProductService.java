cb611a5579297fd47e92c3c105b04714ec28bb47
Ajay Gathadi <ajaygathadi@gmail.com>
2026-06-04 17:07:36 +0530

    Add deleteProduct method

--- src/main/java/com/gathadi/ajay/ecommerce/service/ProductService.java
+++ src/main/java/com/gathadi/ajay/ecommerce/service/ProductService.java
@@ -16,4 +16,6 @@ public interface ProductService {
     ProductResponse searchProductsByKeyword(String keyword);
 
     ProductDTO updateProduct(Product product, Long productId);
+
+    ProductDTO deleteProduct(Long productId);
 }
