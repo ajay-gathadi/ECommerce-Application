b0df9d89668f6b8e78666c5f91b45b5dd13082ac
Ajay Gathadi <ajaygathadi@gmail.com>
2026-06-04 15:18:51 +0530

    Add updateProduct method

--- src/main/java/com/gathadi/ajay/ecommerce/service/ProductService.java
+++ src/main/java/com/gathadi/ajay/ecommerce/service/ProductService.java
@@ -14,4 +14,6 @@ public interface ProductService {
     ProductResponse getProductsByCategory(Long categoryId);
 
     ProductResponse searchProductsByKeyword(String keyword);
+
+    ProductDTO updateProduct(Product product, Long productId);
 }
