f0df10be3e0392473f4c8616595e870cc3d3dba8
Ajay Gathadi <ajaygathadi@gmail.com>
2026-06-05 13:58:47 +0530

    Update the Product entity parameter to the ProductDTO type with the intent of loose coupling

--- src/main/java/com/gathadi/ajay/ecommerce/controller/ProductController.java
+++ src/main/java/com/gathadi/ajay/ecommerce/controller/ProductController.java
@@ -13,42 +13,40 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class ProductController {
     @Autowired
-    private ProductService  productService;
+    private ProductService productService;
 
     @PostMapping("/admin/categories/{categoryId}/product")
-    public ResponseEntity<ProductDTO> addProduct(@RequestBody Product product, @PathVariable Long categoryId){
-
-        ProductDTO productToBeSaved = productService.addProduct(product, categoryId);
-
+    public ResponseEntity<ProductDTO> addProduct(@RequestBody ProductDTO productDTO, @PathVariable Long categoryId) {
+        ProductDTO productToBeSaved = productService.addProduct(productDTO, categoryId);
         return new ResponseEntity<>(productToBeSaved, HttpStatus.CREATED);
     }
 
     @GetMapping("/public/products")
-    public ResponseEntity<ProductResponse> getAllProducts(){
+    public ResponseEntity<ProductResponse> getAllProducts() {
         ProductResponse productResponse = productService.getAllProducts();
         return new ResponseEntity<>(productResponse, HttpStatus.OK);
     }
 
     @GetMapping("/public/categories/{categoryId}/products")
-    public ResponseEntity<ProductResponse> getProductsByCategory(@PathVariable Long categoryId){
+    public ResponseEntity<ProductResponse> getProductsByCategory(@PathVariable Long categoryId) {
         ProductResponse productResponse = productService.getProductsByCategory(categoryId);
         return new ResponseEntity<>(productResponse, HttpStatus.OK);
     }
 
     @GetMapping("/public/products/keyword/{keyword}")
-    public ResponseEntity<ProductResponse> searchProductsByKeyword(@PathVariable String keyword){
+    public ResponseEntity<ProductResponse> searchProductsByKeyword(@PathVariable String keyword) {
         ProductResponse productResponse = productService.searchProductsByKeyword(keyword);
         return new ResponseEntity<>(productResponse, HttpStatus.OK);
     }
 
     @PutMapping("/admin/product/{productId}")
-    public ResponseEntity<ProductDTO> updateProduct(@RequestBody Product product, @PathVariable Long productId){
+    public ResponseEntity<ProductDTO> updateProduct(@RequestBody Product product, @PathVariable Long productId) {
         ProductDTO updatedProduct = productService.updateProduct(product, productId);
         return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
     }
 
     @DeleteMapping("/admin/product/{productId}")
-    public ResponseEntity<ProductDTO> deleteProduct(@PathVariable Long productId){
+    public ResponseEntity<ProductDTO> deleteProduct(@PathVariable Long productId) {
         ProductDTO deletedProduct = productService.deleteProduct(productId);
         return new ResponseEntity<>(deletedProduct, HttpStatus.OK);
     }
