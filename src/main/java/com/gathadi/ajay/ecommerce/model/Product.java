09f4a385e06b85d715630f4e8b80bffe25ae89f8
Ajay Gathadi <ajaygathadi@gmail.com>
2026-06-05 13:59:25 +0530

    Update the 'discount' field to the 'productDiscount'

--- src/main/java/com/gathadi/ajay/ecommerce/model/Product.java
+++ src/main/java/com/gathadi/ajay/ecommerce/model/Product.java
@@ -18,7 +18,7 @@ public class Product {
     private String productDescription;
     private Integer productQuantity;
     private double productPrice;
-    private double discount;
+    private double productDiscount;
     private double productSpecialPrice;
 
     @ManyToOne
