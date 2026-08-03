5b3c3327ec370bccd1ccd8d75313d3d03bf539ef
Ajay Gathadi <ajaygathadi@gmail.com>
2026-08-03 15:37:49 +0530

    refactor(model): Annotate with @ToString to generate the toString method implementation

--- src/main/java/com/gathadi/ajay/ecommerce/model/Role.java
+++ src/main/java/com/gathadi/ajay/ecommerce/model/Role.java
@@ -4,11 +4,13 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "roles")
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 public class Role {
     @Id
